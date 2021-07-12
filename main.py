5de8b5f9938af2212d974a1f1f775d8079938683
chineseholmes <chineseholmes@gmail.com>
2021-07-12 21:58:41 +0800

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ PROXIES = {
 }
 
 
-def login(username: str, password: str) -> (str, requests.session):
+def login(username, password) -> (str, requests.session):
     headers = {
         "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
                       "Chrome/83.0.4103.116 Safari/537.36",
